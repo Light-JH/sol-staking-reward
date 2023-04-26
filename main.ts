2a970a744a69c39ed48e8bd44cb4f0c4849b7b53
Light-JH <phoebehu@outlook.com>
2023-04-26 15:35:14 -0700

    CLI

--- main.ts
+++ main.ts
@@ -1,2 +1,19 @@
-let message: string = "Hello world";
-console.log(message);
+import { PublicKey } from '@solana/web3.js';
+
+class CLI {
+    address: string;
+
+    constructor() {
+        if (process.argv.length < 3) {
+            throw new Error('Missing address argument');
+        }
+        this.address = process.argv[2];
+    }
+}
+
+try {
+    const cli = new CLI();
+    console.log(`The address saved is: ${cli.address}`);
+} catch (error: any) {
+    console.error(error.message);
+}
